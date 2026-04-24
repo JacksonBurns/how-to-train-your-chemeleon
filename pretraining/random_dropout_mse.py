9d9ff7887a908472bb6b21927cdb24e719d773b1
JacksonBurns <jwburns@mit.edu>
2026-04-24 09:26:06 -0400

    flip mask definition

--- pretraining/random_dropout_mse.py
+++ pretraining/random_dropout_mse.py
@@ -17,6 +17,6 @@ class RandomDropoutMSE(MSE):
         gt_mask: torch.Tensor | None = None,
     ) -> None:
         # overrides parent to generate a randomly initialized mask
-        random_mask = (torch.rand_like(targets) < DROPOUT_FRACTION).bool()
+        random_mask = (torch.rand_like(targets) > DROPOUT_FRACTION).bool()
         mask = random_mask if mask is None else torch.logical_or(random_mask, mask)  # i.e., ignore if either mask requests so
         super().update(preds, targets, mask, weights, lt_mask, gt_mask)

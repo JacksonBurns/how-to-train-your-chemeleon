89d903b821ef1e1008e1c6d3e0a081c5811f1be0
JacksonBurns <jwburns@mit.edu>
2026-05-13 10:15:06 -0400

    add script for extracting MP from ckpt

---
+++ pretraining/extract_mp.py
@@ -0,0 +1,23 @@
+# extract_mp.py
+import sys
+from pathlib import Path
+
+import torch
+
+from train import MPNN  # import from here to trigger other imports
+
+
+try:
+    ckpt = Path(sys.argv[1])
+except:
+    print("usage: python extract_mp.py /path/to/model.ckpt")
+    exit(1)
+
+out = ckpt.parent.resolve() / (ckpt.stem + "_mp.pt")
+print(f"output will be written to '{out}'")
+if out.exists():
+    print("output file already exists - exiting.")
+    exit(1)
+
+m = MPNN.load_from_checkpoint(ckpt, map_location="cpu")
+torch.save({"hparams": m.message_passing.hparams, "state_dict": m.message_passing.state_dict()}, out)

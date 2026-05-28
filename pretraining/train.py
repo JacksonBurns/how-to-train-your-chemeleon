64a22d0b20c5a27559eab6f27b74569638c99d67
JacksonBurns <jwburns@mit.edu>
2026-05-28 16:12:09 -0400

    Experiment 19: Set bias=True in MPNN

--- pretraining/train.py
+++ pretraining/train.py
@@ -276,6 +276,7 @@ if __name__ == "__main__":
             d_e=featurizer.bond_fdim,
             d_h=2_048,
             depth=4,
+            bias=True,
             activation=torch.nn.GELU(),
     )
 
